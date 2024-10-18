da84aec9e6c93d1369cf759d5be6abb586a548d6
tct <27ttruebe@derryfield.org>
2024-10-18 08:50:18 -0400

    Created HW_08_PartA

---
+++ Unit_04/HW_08/HW_08_PartA.java
@@ -0,0 +1,14 @@
+/*
+ * Theodore Truebe
+ * HW Part
+ */
+package Unit_04.HW_08;
+import java.util.Scanner;
+public class HW_08_PartA{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        in.close();
+    }
+}
