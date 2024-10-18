b4ae299432007ffd3d468a0bc6454a88d6e9176a
tct <27ttruebe@derryfield.org>
2024-10-18 08:08:41 -0400

    Create Adv03_Challenge_Problems.java

---
+++ Unit_02/Adv03_Challenge_Problems.java
@@ -0,0 +1,12 @@
+/*
+ * Theodore Truebe
+ * HW03
+ */
+import java.util.Scanner;
+public class Adv03_Challenge_Problems{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        in.close();
+    }
+}
