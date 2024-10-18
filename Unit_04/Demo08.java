3d6a1c11c6b79fc2c70773fdbcc2fb2a5208a489
tct <27ttruebe@derryfield.org>
2024-10-18 08:50:31 -0400

    Made it have loops in it

--- Unit_04/Demo08.java
+++ Unit_04/Demo08.java
@@ -9,9 +9,32 @@ public class Demo08 {
         Scanner in = new Scanner(System.in);
         //loops printing out a number until the number is 5
         int x = 1;
-        while (x!=6){
+        while (x < 6){
             System.out.println("The number is " + x);
-            x+=1;
+            x += 1;
+        }
+        //Infinite loop (Would be infinite without the if statement)
+        int i = 1;
+        while (i>0){
+            System.out.println(i);
+            i+=1;
+            // stops it at 10,000 so I don't destroy my computer
+            if (i > 10000){
+                i = 0;
+            }
+        }
+        double total = 0.0;
+        while (true) {
+            System.out.println("Enter a number or 'q' to quit: ");
+            //does it have a q or Q?
+            if (in.hasNext("q")|| in.hasNext("Q")){
+                in.next();
+                break;
+            } else {
+                if (in.hasNextDouble()){
+                    total += in.nextDouble();
+                }
+            }
         }
         in.close();
     }
