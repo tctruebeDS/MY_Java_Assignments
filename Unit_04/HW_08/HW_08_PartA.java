243d6aef1e6bd6f580c5ac383361fbe6344e7075
tct <27ttruebe@derryfield.org>
2024-10-18 09:12:31 -0400

    Made the loop
    Looped dividing the values by 2 and added 30 years every time it got divided by 2

--- Unit_04/HW_08/HW_08_PartA.java
+++ Unit_04/HW_08/HW_08_PartA.java
@@ -1,6 +1,6 @@
 /*
  * Theodore Truebe
- * HW Part
+ * HW08 PartA
  */
 package Unit_04.HW_08;
 import java.util.Scanner;
@@ -8,7 +8,17 @@ public class HW_08_PartA{
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        //defines the starting values
+        double percentage = 100.00;
+        int years = 0; 
+        // makes the while loop occur while the percentage is greater than 1
+        while (percentage > 1){
+            //makes 30 years pass and the total percentage gets divided by 2
+            percentage = percentage / 2.0;
+            years += 30;
+        }
+        // outputs the final values
+        System.out.println(years + " years have passed and " + percentage + "% of the material is left.");
         in.close();
     }
 }
