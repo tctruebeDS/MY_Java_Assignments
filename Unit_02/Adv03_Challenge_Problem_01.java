bf5de2006f46962774f283414d071b54c1c18489
tct <27ttruebe@derryfield.org>
2024-10-21 11:26:40 -0400

    Made it mostly work
    There is a bug where if the double has more than 2 decimal places, which it should really not, the amount of cents is 1 above what it should be

--- Unit_02/Adv03_Challenge_Problem_01.java
+++ Unit_02/Adv03_Challenge_Problem_01.java
@@ -8,18 +8,50 @@
 *Calculates how many dollars and cents the user has (for example, 21 dollars and 74 cents).
 *Displays this information to the user in an easy to understand format.
  */
+//WORD OF ADVICE: DON'T RUN ABNORMALLY LARGE NUMBERS. IT TAKES A LONG TIME. 
 import java.util.Scanner;
 
 public class Adv03_Challenge_Problem_01 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the price in dollars and cents format (For example, 21.74 represents twenty one dollars and 74 cents) :");
-        double price = in.nextDouble();
-        int dollars = 0;
-        while (price > 1){
-            price -= 1;
-            
+        double price = 0.00;
+        //gets user input
+
+        //makes it so multiple numbers can be run without having to restart the program
+        while (true) {
+                System.out.print("Enter the price in dollars and cents format (For example, 21.74 represents twenty one dollars and 74 cents) or 'q' to quit: ");
+                // does it have a q or Q? If so, stop the program
+                if (in.hasNext("q") || in.hasNext("Q")) {
+                    in.next();
+                    break;
+                } else {
+                    if (in.hasNextDouble()) {
+                        price = in.nextDouble();
+                    }
+                }
+            // does the maths
+            int dollars = 0;
+            int cents = 0;
+            //Figures out how many dollars are there
+            while (price >= 1){
+                price -= 1;
+                dollars += 1;
+            }
+            //figures out how many cents are there
+            while (price > .001){
+                price -= .01;
+                cents += 1;
+            }
+            //OUTPUT
+            if (dollars > 0 && cents > 0){ //when there are dollars and cents
+                System.out.println("You have " + dollars + " dollars and " + cents + " cents.");
+            } else if (dollars > 0){ // when there are just dollars
+                System.out.println("You have " + dollars + " dollars.");
+            } else { // when there are just cents
+                System.out.println("You have " + cents + " cents.");
+            }
         }
         in.close();
+        
+        }
     }
-}
