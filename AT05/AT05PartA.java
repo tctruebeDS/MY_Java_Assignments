269803233ba8fa5771c40950f71152c6a4083b02
tct <27ttruebe@derryfield.org>
2024-10-23 12:20:56 -0400

    Setup AT05 part A

---
+++ AT05/AT05PartA.java
@@ -0,0 +1,14 @@
+package AT05;
+ /*
+ * Theodore Truebe
+ * AT05 HW Part A
+ */
+import java.util.Scanner;
+public class AT05PartA { 
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        in.close();
+    }
+}
