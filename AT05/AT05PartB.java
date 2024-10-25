0a59bc70c87dfd483ca9bfcafbd483fcd794a167
tct <27ttruebe@derryfield.org>
2024-10-25 08:15:48 -0400

    Created a program that does not work

--- AT05/AT05PartB.java
+++ AT05/AT05PartB.java
@@ -4,11 +4,72 @@ package AT05;
  * HW AT05 Part B
  */
 import java.util.Scanner;
+
+class Light {
+    // track the state of the light.
+    // true means the light is on.
+    // false means the light is off.
+    boolean state = false;
+
+    // this method returns true if the light is on.
+    public boolean view() {
+        return state;
+    }
+
+    // turn on light.
+    public void turnOn() {
+        state = true;
+    }
+
+    // turn off light.
+    public void turnOff() {
+        state = false;
+    }
+}
+
 public class AT05PartB {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
-        in.close();
+        var running = true;
+        int lightBright = 0; 
+        while (running==true){
+            System.out.println("enter q to quit");
+
+            Light light = new Light();
+            if(in.hasNext("q")| in.hasNext("Q")){
+                running = false;
+            } else if (in.hasNext("view")){
+                // print current state.
+                if (lightBright != 0) {
+                    System.out.print(lightBright);
+                }
+                System.out.println("state = " + light.view());
+            } else if (in.hasNextInt()){
+                lightBright=in.nextInt();
+            } else if (in.hasNext("up")&& lightBright <11){
+                lightBright +=1;
+            } else if (in.hasNext("down")&& lightBright > 1){
+                lightBright-=1;
+            } else if (in.hasNext("on")){
+                light.turnOn();            
+            } else if (in.hasNext("off")){
+                light.turnOff();
+            } else {
+                System.err.println("error.");
+                running=false;
+            }
+            // create a light object.
+
+            // turn on the light.
+
+
+            // print current state.
+
+
+            // turn off the light.
+            
+            in.close();
+        }
     }    
 }
