9e4ae324a2249d7823f86427fd617020085071c6
tct <27ttruebe@derryfield.org>
2024-10-25 07:59:48 -0400

    Finished the code

--- AT05/AT05PartA.java
+++ AT05/AT05PartA.java
@@ -1,14 +1,18 @@
 package AT05;
- /*
- * Theodore Truebe
- * AT05 HW Part A
- */
-import java.util.Scanner;
-public class AT05PartA { 
+
+/*
+* Theodore Truebe
+* AT05 HW Part A
+*/
+import java.util.Random;
+
+public class AT05PartA {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        in.close();
+        Random rand = new Random();
+        for (int i = 0; i < 100; i += 1) {
+            int num = rand.nextInt(0, 100);
+            System.out.println(num);
+        }
     }
 }
