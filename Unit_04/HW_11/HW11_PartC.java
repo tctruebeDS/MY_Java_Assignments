04977b347c6f2d8f72146bdc2d7acc37aa45a737
tct <27ttruebe@derryfield.org>
2024-11-06 10:26:23 -0500

    Created a working HW11 Part C
    It works and calculates the volume of  a sphere

---
+++ Unit_04/HW_11/HW11_PartC.java
@@ -0,0 +1,24 @@
+
+/*
+ * Theodore Truebe
+ * HW 11 Part C
+ */
+import java.util.Scanner;
+
+public class HW11_PartC {
+    public static double getVolume(double radius){
+        //does the math
+        double volume = (radius * radius * radius) * (4.0 / 3.0) * Math.PI;
+        return volume;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //gets user input
+        System.out.print("Enter a radius: ");
+        double radius = in.nextDouble();
+        //formatted output so it only goes to 2 decimal spaces
+        System.out.printf("The volume of your sphere with a radius of %.2f is %.2f",radius, getVolume(radius));
+        in.close();
+    }
+}
