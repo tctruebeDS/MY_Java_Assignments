1ac0bf773f027290b353a195671e73c143bc6ac6
tct <27ttruebe@derryfield.org>
2024-11-06 10:28:04 -0500

    Used this to step through the program

---
+++ Unit_04/HW_11/HW11_PartA.java
@@ -0,0 +1,19 @@
+//Theodore Truebe
+//HW11 Part A, used this to watch the variables
+public class HW11_PartA{
+
+public static void main(String[] args)
+{
+   double len = 2;
+   double result = cubeVolume(len);
+   System.out.println(result);
+   len = 10;
+   result = cubeVolume(len);
+   System.out.println(result);
+}
+public static double cubeVolume(double sideLength)
+{   
+   double volume = sideLength * sideLength * sideLength;
+   return volume;
+}
+}
