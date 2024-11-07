5496017888d60bc5ce3f3d648e5062d4e7c20e0d
tct <27ttruebe@derryfield.org>
2024-11-07 10:19:24 -0500

    Created AT07 Part B
    It mostly works, one minor bug where the text on line 77 outputs twice

---
+++ Unit05/AT07_PartB.java
@@ -0,0 +1,112 @@
+package Unit05;
+
+import java.util.Scanner;
+/*Theodore Truebe
+ * AT07 Part B
+ */
+
+/*Get x coordinate value
+*Get y coordinate value
+*Get r (radius) value (polar coordinate)
+*Get angle value (polar coordinate)
+*Translate (dx,dy) - translate the point dx and dy
+*Distance to (point) - returns the distance to another point; takes another point as an argument.
+ */
+public class AT07_PartB {
+    public static double valueX;
+    public static double valueY;
+    public static double moveX;
+    public static double moveY;
+    public static double newX;
+    public static double newY;
+
+    public static double getAngle() {
+        double theta = Math.atan(valueY / valueX);
+        // calculates theta
+        theta = Math.toDegrees(theta);
+        return theta;
+
+    }
+
+    public static double getRad() {
+        // calculates the radius
+        double rad = Math.sqrt((valueX * valueX) + (valueY * valueY));
+        return rad;
+    }
+
+    public static double translateX() {
+        // makes something for translating the point by an x value
+        valueX = valueX + moveX;
+        double rad = Math.sqrt((valueX * valueX) + (valueY * valueY));
+        return rad;
+    }
+
+    public static double translateY() {
+        // makes something for translating the point by a Y value
+        valueY = valueY + moveY;
+        double rad = Math.sqrt((valueX * valueX) + (valueY * valueY));
+        return rad;
+    }
+
+    public static double distanceTo() {
+        // finds the distance between the 2 points
+        double totalDistance = Math.sqrt(((newX - valueX) * (newX - valueX)) + ((newY - valueY) * (newY - valueY)));
+        return totalDistance;
+    }
+
+    public static void thetaRad() {
+        // outputs theta and the radius
+        System.out.printf("Theta = %.2f%n", getAngle());
+        System.out.printf("Radius = %.2f%n", getRad());
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean running = true;
+        System.out.print("Enter an X coordinate: ");
+        valueX = in.nextDouble();
+        System.out.print("Enter a Y coordinate: ");
+        valueY = in.nextDouble();
+        // outputs theta and the radius
+        thetaRad();
+        // makes a loop so the user can do different things without having to restart
+        // the program
+        while (running) {
+
+            System.out.print(
+                    "Enter X to translate the point by an X value, enter Y to translate the point by a Y value, enter N to add another point, or enter Q to quit: ");
+            String input = in.nextLine();
+            input = input.toLowerCase();
+            // finds out what the user wants to do
+            if (input.equals("x")) {
+                System.out.print("Enter the value that you would like to translate the X by: ");
+                moveX = in.nextDouble();
+                translateX();
+                // outputs theta and the radius
+                thetaRad();
+            } else if (input.equals("y")) {
+                System.out.print("Enter the value that you would like to translate the Y by: ");
+                moveY = in.nextDouble();
+                translateY();
+                thetaRad();
+                // outputs theta and the radius
+            } else if (input.equals("n")) {
+                System.out.print("Enter the X coordinate for the new point: ");
+                newX = in.nextDouble();
+                System.out.print("Enter the Y coordinate for the new point: ");
+                newY = in.nextDouble();
+                System.out.printf("The distance between the two points = %.2f%n", distanceTo());
+            } else if (input.equals("q")) {
+                break;
+            } else if (input.equals("")) {
+                // does nothing if the input is blank
+            } else {
+                //
+                System.err.println("Error.");
+            }
+        }
+
+        in.close();
+    }
+
+}
