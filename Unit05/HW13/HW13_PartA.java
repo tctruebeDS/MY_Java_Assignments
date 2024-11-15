5d7e1dc1686e6953a340bf39d6f52226211cf49a
tct <27ttruebe@derryfield.org>
2024-11-15 17:14:08 -0500

    Smiley Face- HW13 Part A

---
+++ Unit05/HW13/HW13_PartA.java
@@ -0,0 +1,95 @@
+package Unit05.HW13;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.*;
+
+public class HW13_PartA {
+    /*
+     * Change the following method to customize
+     * what is drawn in the JFrame.
+     */
+    public static void draw(Graphics g) {
+        // draws a face that is looking down and to the right of the user
+        
+        g.setColor(Color.YELLOW);
+        g.fillOval(0, 0, 400, 400);
+        g.setColor(Color.BLACK);
+        g.drawOval(0, 0, 400, 400);
+        g.drawLine(100, 300, 300, 300);
+        g.drawOval(150, 75, 50, 50);
+        g.drawOval(250, 75, 50, 50);
+        g.fillOval(175, 90, 25, 25);
+        g.fillOval(275, 90, 25, 25);
+    }
+
+    public static enum OS {
+        WIN, MAC, LINUX, OTHER
+    }
+
+    public static OS checkOS() {
+        OS val;
+        String name = System.getProperty("os.name").toLowerCase();
+        if (name.indexOf("win") >= 0) {
+            val = OS.WIN;
+        } else if (name.indexOf("mac") >= 0) {
+            val = OS.MAC;
+        } else if (name.indexOf("nux") >= 0) {
+            val = OS.LINUX;
+        } else {
+            val = OS.OTHER;
+        }
+
+        return val;
+    }
+
+    // Do not modify the code in the main method.
+    // Your code will go into the draw method above.
+    public static void main(String[] args) {
+        // this is the desired dimensions of the content area
+        // of our JFrame window.
+        final int CONTENT_WIDTH = 400;
+        final int CONTENT_HEIGHT = 400;
+
+        // the values for border width and title bar height
+        // depend on the operating system.
+        int border_width = 0;
+        int bar_height = 0;
+        OS os = checkOS();
+        switch (os) {
+            case WIN:
+                border_width = 7;
+                bar_height = 30;
+                break;
+            case MAC:
+                border_width = 0;
+                bar_height = 28;
+                break;
+            case LINUX:
+                // I don't know what the values are for Linux.
+                break;
+            case OTHER:
+                break;
+        }
+
+        int frame_width = CONTENT_WIDTH + 2 * border_width;
+        int frame_height = CONTENT_HEIGHT + bar_height + border_width;
+        JFrame frame = new JFrame();
+        frame.setSize(frame_width, frame_height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JComponent component = new JComponent() {
+            public void paintComponent(Graphics graph) {
+                draw(graph);
+            }
+        };
+
+        frame.add(component);
+        frame.setVisible(true);
+
+        System.out.println("Frame Size   : " + frame.getSize());
+        System.out.println("Frame Insets : " + frame.getInsets());
+        System.out.println("Content Size : " + frame.getContentPane().getSize());
+    }
+
+}
