2b1db13aab1b949a1f8daf113758595d9151b761
tct <27ttruebe@derryfield.org>
2024-11-18 16:42:37 -0500

    HW 13 Part C
    Flower

---
+++ Unit05/HW13/HW13_PartC.java
@@ -0,0 +1,108 @@
+package Unit05.HW13;
+
+// Theodore Truebe
+// Unit 05 HW 13 Part C
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.*;
+
+public class HW13_PartC {
+    /*
+     * Change the following method to customize
+     * what is drawn in the JFrame.
+     */
+    static int scale = 150;
+
+    public static void draw(Graphics g) {
+        final double TWOPI = 2 * Math.PI;
+        final double RESOLUTION = 1000;
+        double delta= (TWOPI) / RESOLUTION;
+        double theta = 0; 
+        
+        while (theta <= TWOPI) {
+            double r = Math.cos(2* theta);
+            int xVal = (int)(100*r  * Math.cos(theta));         
+            int yVal = (int)(100*r  * Math.sin(theta));    
+            xVal= xVal + 200;
+            yVal= yVal + 200;
+            g.setColor(Color.BLUE);
+            g.fillOval(xVal, yVal, 2, 2);
+            theta += delta;
+            /* r=hyp, theta = angle
+            *costheta = adj/hyp, so x = r*costheta
+            *theta=independant, from 0 to 2pi rad
+            */
+        }
+        
+    }
+
+    public static enum OS {
+        WIN, MAC, LINUX, OTHER
+    }
+
+    public static OS checkOS() {
+        OS val;
+        String name = System.getProperty("os.name").toLowerCase();
+        if (name.indexOf("win") >= 0) {
+            val = OS.WIN;
+        } else if (name.indexOf("mac") >= 0) {
+            val = OS.MAC;
+        } else if (name.indexOf("nux") >= 0) {
+            val = OS.LINUX;
+        } else {
+            val = OS.OTHER;
+        }
+
+        return val;
+    }
+
+    // Do not modify the code in the main method.
+    // Your code will go into the draw method above.
+    public static void main(String[] args) {
+        // this is the desired dimensions of the content area
+        // of our JFrame window.
+        final int CONTENT_WIDTH = 400;
+        final int CONTENT_HEIGHT = 400;
+
+        // the values for border width and title bar height
+        // depend on the operating system.
+        int border_width = 0;
+        int bar_height = 0;
+        OS os = checkOS();
+        switch (os) {
+            case WIN:
+                border_width = 7;
+                bar_height = 30;
+                break;
+            case MAC:
+                border_width = 0;
+                bar_height = 28;
+                break;
+            case LINUX:
+                // I don't know what the values are for Linux.
+                break;
+            case OTHER:
+                break;
+        }
+
+        int frame_width = CONTENT_WIDTH + 2 * border_width;
+        int frame_height = CONTENT_HEIGHT + bar_height + border_width;
+        JFrame frame = new JFrame();
+        frame.setSize(frame_width, frame_height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JComponent component = new JComponent() {
+            public void paintComponent(Graphics graph) {
+                draw(graph);
+            }
+        };
+
+        frame.add(component);
+        frame.setVisible(true);
+
+        System.out.println("Frame Size   : " + frame.getSize());
+        System.out.println("Frame Insets : " + frame.getInsets());
+        System.out.println("Content Size : " + frame.getContentPane().getSize());
+    }
+
+}
