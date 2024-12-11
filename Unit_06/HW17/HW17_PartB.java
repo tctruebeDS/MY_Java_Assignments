1be4abdecccf03b44f0c5ede81a636ab8803e288
tct <27ttruebe@derryfield.org>
2024-12-11 10:01:03 -0500

    Update HW17_PartB.java

--- Unit_06/HW17/HW17_PartB.java
+++ Unit_06/HW17/HW17_PartB.java
@@ -1,25 +1,15 @@
 package HW17;
 
 public class HW17_PartB {
-    public static void printArray(int[][] array1, int[][] array2) {
-        System.out.println("First array: ");
-        for (int row = 0; row < array1.length; row++) {
-            for (int col = 0; col < array1[0].length; col++) {
-                System.out.print(array1[row][col]);
-                // only print a comma if it's not the last column.
-                if (col < array1[0].length - 1) {
-                    System.out.print(", ");
-                }
 
-            }
-            System.out.println();
-        }
-        System.out.println("Second array: ");
-        for (int row = 0; row < array2.length; row++) {
-            for (int col = 0; col < array2[0].length; col++) {
-                System.out.print(array2[row][col]);
+
+    public static void printArray(int[][] array) {
+
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array[0].length; col++) {
+                System.out.print(array[row][col]);
                 // only print a comma if it's not the last column.
-                if (col < array2[0].length - 1) {
+                if (col < array[0].length - 1) {
                     System.out.print(", ");
                 }
 
@@ -29,6 +19,7 @@ public class HW17_PartB {
     }
 
     public static void main(String[] args) {
+        // defines arrays
         int[][] array1 = {
                 { 8, 6, 7 },
                 { 5, 3, 0 },
@@ -39,6 +30,11 @@ public class HW17_PartB {
                 { 8, 4, 5, 7 },
 
         };
-        printArray(array1, array2);
+        // prints first array
+        System.out.println("First array: ");
+        printArray(array1);
+        // prints second array
+        System.out.println("Second array: ");
+        printArray(array2);
     }
 }
