702fe0c51754b029d13bf85d0444f0c3fa3cc044
tct <27ttruebe@derryfield.org>
2024-12-11 10:17:29 -0500

    Create HW17_PartA.java

---
+++ Unit_06/HW17/HW17_PartA.java
@@ -0,0 +1,17 @@
+package HW17;
+// Theodore Truebe 
+// Part A
+public class HW17_PartA {
+    public static void main(String[] args) {
+        // First part of part a 
+        int[][] arr = new int[4][3];
+        // Second part of part a 
+        int[][] arg = new int[4][3];
+        // Third part of part A
+        int[][] arrrrg = {
+                { 8, 6, 7 },
+                { 5, 3, 0 },
+        };
+
+    }
+}
