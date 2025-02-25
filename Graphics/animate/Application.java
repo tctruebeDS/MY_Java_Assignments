046bb46f23ae8c373e3361b79c44a86cd164095f
tct <27ttruebe@derryfield.org>
2025-02-25 09:03:06 -0500

    Did AT01 2D Graphics

---
+++ Graphics/animate/Application.java
@@ -0,0 +1,93 @@
+package animate;
+
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+
+public class Application extends JFrame {
+    private final int FRAME_WIDTH = 1080;
+    private final int FRAME_HEIGHT = 720;
+    
+
+    /*
+     * Constructor.
+     */
+    public Application(String title) {
+        // the values for border width and title bar height
+        // depend on the operating system.
+        int border_width = 0;
+        int bar_height = 0;
+        OS os = checkOS();
+        switch (os) {
+            case WIN:
+                border_width = 7;
+                bar_height = 30;
+                break;
+            case MAC:
+                border_width = 0;
+                bar_height = 28;
+                break;
+            case LINUX:
+                // I don't know what the values are for Linux.
+                break;
+            case OTHER:
+                break;
+        }
+        // Use the inherited setSize() method
+       // to set the size of our frame.
+       // this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+
+       // add a Board to our JFrame.
+       Board board = new Board();
+       this.add(board);
+   
+       // set the size based on the board size
+       // and also account for the border width and the bar height.
+       int application_height = (int) board.getPreferredSize().getHeight() + bar_height + border_width;
+       int application_width = (int) board.getPreferredSize().getWidth() + 2 * border_width;
+       this.setSize(application_width, application_height);
+
+        // int application_width = FRAME_WIDTH + 2 * border_width;
+        // int application_height = FRAME_HEIGHT + bar_height + border_width;
+        this.setTitle(title);
+        this.setSize(application_width, application_height);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+    }
+
+    public static enum OS {
+        WIN, MAC, LINUX, OTHER
+    }
+
+    public static OS checkOS() {
+        OS val;
+        String name = System.getProperty("os.name").toLowerCase();
+        if (name.indexOf("win") >= 0) {
+            val = OS.WIN;
+        } else if (name.indexOf("mac") >= 0) {
+            val = OS.MAC;
+        } else if (name.indexOf("nux") >= 0) {
+            val = OS.LINUX;
+        } else {
+            val = OS.OTHER;
+        }
+        return val;
+    }
+
+    public static void main(String[] args) {
+
+        /*
+         * In Java, GUI related actions should be
+         * performed in the AWT event thread
+         * or it could cause severe problems with the GUI.
+         * EventQueue.invokeLater() posts your GUI-related
+         * actions in the AWT event queue to avoid causing
+         * any problems with other GUI-related actions.
+         */
+        EventQueue.invokeLater(() -> {
+            Application myApp = new Application("Not-A-Title");
+            myApp.setVisible(true);
+        });
+
+    }
+}
---
+++ Graphics/animate/Board.java
@@ -0,0 +1,58 @@
+package animate;
+// Theodore Truebe
+// HW A01 Intro to 2D Graphics
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.text.AttributedCharacterIterator;
+
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+    private final int B_WIDTH = 350;
+    private final int B_HEIGHT = 350;
+    private final int SIDE_LEN = 150;
+
+    /*
+     * Constructor
+     */
+    public Board() {
+        setBackground(Color.CYAN);
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+    }
+
+    public void paintComponent(Graphics g) {
+        // call the parent class method.
+        super.paintComponent(g);
+
+        // cast our Graphics object to a Graphics2D object.
+        Graphics2D g2d = (Graphics2D) g;
+        // translate in the x and y directions.
+        int x_t = B_WIDTH / 2;
+        int y_t = B_HEIGHT / 2;
+        x_t = x_t - SIDE_LEN / 2;
+        y_t = y_t - SIDE_LEN / 2;
+        AffineTransform affineTransform = new AffineTransform();
+        affineTransform.translate(x_t, y_t);
+
+        // get the transformed shape.
+        Rectangle rect = new Rectangle(0, 0, SIDE_LEN, SIDE_LEN);
+        affineTransform.rotate(Math.PI/8, x_t, y_t);
+        Shape transformedShape = affineTransform.createTransformedShape(rect);
+
+        // draw the transformed shape on the screen.
+        g2d.setColor(Color.MAGENTA);
+        g2d.fill(transformedShape);
+        
+        g2d.setColor(Color.BLUE);
+        g2d.drawString("Hello, world", 20, 20);
+
+    }
+
+}
