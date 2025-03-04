f0bd42c9d559687faba95ad5aa34f285e7738bf7
tct <27ttruebe@derryfield.org>
2025-03-04 08:19:47 -0500

    Class stuff

---
+++ Graphics/A02/animate/Board.java
@@ -0,0 +1,62 @@
+package A02.animate;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+    private final int B_WIDTH = 350;
+    private final int B_HEIGHT = 350;
+    private final int SIDE_LEN = 150;
+    private BufferedImage img;
+
+    /*
+     * Constructor
+     */
+    public Board() {
+        // set background color of the board and default size.
+        setBackground(Color.CYAN);
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+        // attempt to load the image.
+        try {
+            File imageFile = new File("A02/animate/media/cakes.jpg");
+            img = ImageIO.read(imageFile);
+            setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        AffineTransform affineTransform = new AffineTransform();
+        int x_t = 0, y_t = 0;
+        int x_scaled = 0, y_scaled = 0;
+        double scale = 0.5;
+        if (img != null) {
+            x_t = this.getWidth() / 2;
+            x_scaled = (int) ((img.getWidth() * scale) / 2.0);
+            y_t = this.getHeight() / 2;
+            y_scaled = (int) ((img.getHeight() * scale) / 2.0);
+            affineTransform.translate(x_t - x_scaled, y_t - y_scaled);
+            affineTransform.rotate(Math.toRadians(45), x_scaled, y_scaled);
+            affineTransform.scale(scale, scale);
+            g2d.drawImage(img, affineTransform, null);
+        } else {
+            g2d.setColor(Color.BLUE);
+            g2d.drawString("Unable to load image!", 25, 25);
+        }
+    }
+
+}
